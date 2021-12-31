528966d5a69c70641c9b5a09fbad7f28359583d1
robinwatts96 <robinwatts96@gmail.com>
2021-12-31 06:14:38 +0000

    Starting header & footer

--- themes/bimm-theme/page.php
+++ themes/bimm-theme/page.php
@@ -2,7 +2,7 @@
 
     while (have_posts()) {
         the_post(); ?>
-        <h1>Page, not post.</h1>
+        <h1>Page, not post.</h1> 
         <h2><?php the_title(); ?></h2>
         <?php the_content(); ?>
         <hr>
