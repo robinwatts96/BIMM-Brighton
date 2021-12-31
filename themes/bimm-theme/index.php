960035d2a4578349a0783fe758b09c0842800d45
robinwatts96 <robinwatts96@gmail.com>
2021-12-31 05:55:22 +0000

    Start blog looping

--- themes/bimm-theme/index.php
+++ themes/bimm-theme/index.php
@@ -1,13 +1,5 @@
 <?php 
 
-    $names = array('Rob', 'Brad', 'John', 'Jane', 'Andy');
-    
-    $count = 0;
-    while($count < count($names)) {
-        echo "<li>Hi, my name is $names[$count]</li>";
-        $count++;
-    }
+
 
 ?>
-
-<p>Hi, my name is <?php echo $names[0] ?></p>
