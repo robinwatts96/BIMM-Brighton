b10b8b68373d06c0ec34646ea18be1c715df8afd
robinwatts96 <robinwatts96@gmail.com>
2022-01-02 14:08:14 +0000

    Update index.php

--- themes/bimm-theme/index.php
+++ themes/bimm-theme/index.php
@@ -1,18 +1,5 @@
 <?php get_header(); ?>
 
-<?php  
-  function double_me($num) {
-    return $num * 2;
-  }
-
-  function triple_me($num) {
-    return $num * 3;
-  }
-  
- echo triple_me(double_me(5));
-
-?>
-
 <div class="page-banner">
       <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/library-hero.jpg') ?>)"></div>
       <div class="page-banner__content container t-center c-white">
