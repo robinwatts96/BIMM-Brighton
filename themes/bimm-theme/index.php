38c1e27de6b58ff4e2a56fa8a6d5c99f344aa389
robinwatts96 <robinwatts96@gmail.com>
2022-01-04 07:21:17 +0000

    Heading updated

--- themes/bimm-theme/index.php
+++ themes/bimm-theme/index.php
@@ -16,13 +16,13 @@ get_header();?>
       while (have_posts()) {
         the_post(); ?>
         <div class="post-item">
-          <h2><a href="<?php the_permalink() ?>;"><?php the_title(); ?></a></h2>
+          <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink() ?>;"><?php the_title(); ?></a></h2>
           <div class="metabox">
             <p>Posted by Rob on 13/09/1996 in News</p>
           </div>
           <div class="generic-content">
             <?php the_excerpt(); ?>
-            <p><a href="<?php the_permalink(); ?>">Continue reading</a></p>
+            <p><a class="btn btn--blue" href="<?php the_permalink(); ?>">Continue reading &raquo;</a></p>
           </div>
         </div>
       <?php }
